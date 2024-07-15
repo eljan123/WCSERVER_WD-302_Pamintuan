b042c67c24b38c861217ab8bbd9840d03b5bddc1
eljan123 <108778203+eljan123@users.noreply.github.com>
2024-07-15 16:20:00 +0800

    Initial commit

---
+++ Prelims/Server/routes.js
@@ -0,0 +1,15 @@
+//PAMINTUAN, ELJAN JOEL Y.
+//WD-302
+var log = {
+  root: function (root) {
+    return root;
+  },
+  about: function (about) {
+    return about;
+  },
+  contact: function (contact) {
+    return contact;
+  },
+};
+
+module.exports = log;
---
+++ README.md
@@ -0,0 +1,3 @@
+# stackblitz-starters-wwuuzs
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/eljan123/stackblitz-starters-wwuuzs)
