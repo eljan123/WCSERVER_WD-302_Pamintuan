3998ae1a80bece5279e09fb330a032ef0bab4c7e
eljan123 <eljan.y.pamintuan@gmail.com>
2024-07-24 19:26:17 +0800

    COMMIT

---
+++ Prelims/Server/myGreetings/callGreet.js
@@ -0,0 +1,7 @@
+// Pamintuan, Eljan Joel Y.
+// WD-302
+
+const greet = require ('./greet.js');
+
+console.log(greet('John'));
+console.log(greet('Jane'));
---
+++ Prelims/Server/myGreetings/greet.js
@@ -0,0 +1,8 @@
+// Pamintuan, Eljan Joel Y.
+// WD-302
+
+function greet(name) {
+    return "Hello, " + name;
+}
+
+module.exports = greet;
