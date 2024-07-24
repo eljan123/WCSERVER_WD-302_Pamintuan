d4ff6a6b775c5008f9be1af661a7001dd69483b7
eljan123 <eljan.y.pamintuan@gmail.com>
2024-07-24 19:29:53 +0800

    COMMIT

---
+++ Prelims/addNumbers/add.js
@@ -0,0 +1,9 @@
+// Pamintuan, Eljan Joel Y.
+// WD-302
+
+function add(a, b) {
+    return a + b;
+
+}
+
+module.exports = add;
---
+++ Prelims/addNumbers/app.js
@@ -0,0 +1,7 @@
+// Pamintuan, Eljan Joel Y.
+// WD-302
+
+const add = require('./add.js');
+
+const result = add(10, 10);
+console.log(result);
