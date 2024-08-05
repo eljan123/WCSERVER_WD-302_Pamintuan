57c2f6d728ae3a79cfd109d55bbf189b3373e6d1
Eljan <108778203+eljan123@users.noreply.github.com>
2024-08-05 15:55:14 +0800

    Add files via upload

---
+++ server/server.js
@@ -0,0 +1,12 @@
+// Pamintuan, Eljan Joel Y. | WD-302
+
+var express =  require("Express");
+var app = express();
+
+app.get("/", function(request, response){
+    response.send("Hello World!");
+});
+app.listen(3000, function() {
+    console.log('Server running at http://localhost:3000');
+
+});
